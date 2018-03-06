d6243ddd49dee131a9e532159b33c9357e656caf
Andre Gaines <andrejgaines@gmail.com>
2018-03-05 20:19:12 -0700

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace dotnetcoreapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your first name: ");
+            var firstName = Console.ReadLine();
+
+            Console.WriteLine($"Hello {firstName}, welcome to .NET Core.");
+        }
+    }
+}
